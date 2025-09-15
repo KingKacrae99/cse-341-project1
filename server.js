ee26b7b71617698e565f5d89e79de23a95a61c18
KingKacrae99 <kacrae9@gmail.com>
2025-09-15 16:09:31 +0100

    Contacts project updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 app
-  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(bodyParser.json())
 /**********************************************
 * Routing
